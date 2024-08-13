737710a56b3def458752659e5bc323d431a5c724
ybadaoui-ostorlab <youssef.badaoui@ostorlab.dev>
2024-08-13 14:44:52 +0100

    default scheme if scheme is None

--- agent/targets_preparer.py
+++ agent/targets_preparer.py
@@ -99,7 +99,7 @@ def prepare_targets(message: m.Message) -> Generator[definitions.Target, None, N
         parsed_url = urlparse(url)
         host = parsed_url.hostname
         port = parsed_url.port or _get_port(message, parsed_url.scheme)
-        scheme = parsed_url.scheme
+        scheme = parsed_url.scheme or "https"
         if host is not None and port is not None and scheme is not None:
             yield (definitions.Target(host=host, port=port, scheme=scheme))
     else:
