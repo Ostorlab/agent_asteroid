d3fe9f971d63109d01ebb2ecc843460ac98b5c11
burogurama <mohamed.nasser@ostorlab.dev>
2026-05-06 18:20:35 +0100

    bump python version to 3.14

--- agent/importer.py
+++ agent/importer.py
@@ -1,5 +1,6 @@
 """Dynamic importer to enable registry pattern."""
 
+import importlib
 import logging
 import sys
 import traceback
@@ -14,12 +15,15 @@ def importer_for(path: str, prefix: str) -> Callable[[], None]:
     """
     Creates a function for importing all Python files in a specified folder and adding them as attributes
     to a given module.
+
     Args:
         path (str): the path to the folder containing the Python files to import
         prefix (str): the prefix to use when adding the imported files as attributes to the module
+
     Returns:
         A function that can be called to perform the import and attribute assignment. The function takes two
         optional arguments:
+
         - file_path (str): the path to the folder containing the Python files to import (default is `path`)
         - stop_on_error (bool): whether to stop execution if an error occurs during import (default is `True`)
     """
@@ -27,6 +31,7 @@ def importer_for(path: str, prefix: str) -> Callable[[], None]:
     def import_all(path: str = path, stop_on_error: bool = True) -> None:
         """
         Imports all Python files in a specified folder and adds them as attributes to a given module.
+
         Args:
             file_path (str): the path to the folder containing the Python files to import
              (default is the value passed to `importer_for`)
@@ -34,20 +39,25 @@ def importer_for(path: str, prefix: str) -> Callable[[], None]:
         """
         folder = os.path.dirname(path)
         module = sys.modules[prefix]
-        for importer, name, _ in pkgutil.iter_modules([folder]):
+
+        for _, name, _ in pkgutil.iter_modules([folder]):
             absname = prefix + "." + name
+
             if absname in sys.modules:
                 continue
-            loader = importer.find_module(absname)  # type: ignore
+
             try:
-                if loader is not None:
-                    submod = loader.load_module(absname)
+                submod = importlib.import_module(absname)
             except ImportError as e:
                 if stop_on_error:
                     raise
                 # This is for debugging to print the full trace and pinpoint to source of the exception.
                 traceback.print_exc()
-                logger.warning("Cannot load REbus plugin [%s]. Root cause: %s", name, e)
+                logger.warning(
+                    "Cannot load REbus plugin [%s]. Root cause: %s",
+                    name,
+                    e,
+                )
             else:
                 setattr(module, name, submod)
 
